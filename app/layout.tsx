2983d73d0ed6c71c5e0585152b3ed0980e28fabb
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-25 13:58:29 +0100

    feat: Display Modal comp in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import ClientOnly from './components/ClientOnly'
 import Navbar from './components/navbar/Navbar'
+import Modal from './components/modals/Modal';
 import './globals.css'
 import { Nunito } from 'next/font/google'
 
@@ -19,6 +20,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <Modal 
+            isOpen 
+            title="Title"
+            body={<div>Body</div>} 
+          />
           <Navbar />
         </ClientOnly>
         {children}
