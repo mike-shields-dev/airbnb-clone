0c5c0416077c76493f366819ab8ee7e657cf2bfb
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-25 08:38:17 +0100

    chore: Add a Container comp to Navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import Container from '../container/Container';
 
 const Navbar = () => {
   return <div className="fixed w-full bg-white z-10 shadow-sm">
     <div className="py-4 border-b-[1px]">
+      <Container>
+      </Container>
     </div>
   </div>;
 };
