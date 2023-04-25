a3200c59a50758c1e164675e55c0355879c2e446
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-25 10:48:44 +0100

    refactor: Format file

--- app/components/container/Container.tsx
+++ app/components/container/Container.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 interface ContainerProps {
   children: React.ReactNode;
@@ -6,14 +6,16 @@ interface ContainerProps {
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
-    <div className="
+    <div
+      className="
       max-w-[2520px]
       mx-auto
       xl:px-20
       md:px-10
       sm:px-2
       px-4
-    ">
+    "
+    >
       {children}
     </div>
   );
